028df53f86194eb6634768f86c27049ad0e98f4f
Abumeragan <rodrigo.uba@gmail.com>
2015-09-22 20:08:04 -0300

    Separado metodo before para do padrao Strategy

--- test/com/edupsousa/designpatterns/strategy/StrategyTest.java
+++ test/com/edupsousa/designpatterns/strategy/StrategyTest.java
@@ -2,19 +2,28 @@ package com.edupsousa.designpatterns.strategy;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StrategyTest {
 
+	ClasseComStrategy objeto;
+
+	@Before
+	public void instanciarClasse() {
+		objeto = new ClasseComStrategy();
+	}
+
 	@Test
 	public void testarPadraoStrategy() {
-		ClasseComStrategy objeto = new ClasseComStrategy();
-		
+
 		objeto.defineStrategy(new StrategyConcretaA());
-		assertEquals("Ação realizada por StrategyConcretaA", objeto.usaStrategyParaFazerAlgo());
-		
+		assertEquals("Ação realizada por StrategyConcretaA",
+				objeto.usaStrategyParaFazerAlgo());
+
 		objeto.defineStrategy(new StrategyConcretaB());
-		assertEquals("Ação realizada por StrategyConcretaB", objeto.usaStrategyParaFazerAlgo());
+		assertEquals("Ação realizada por StrategyConcretaB",
+				objeto.usaStrategyParaFazerAlgo());
 	}
 
 }
